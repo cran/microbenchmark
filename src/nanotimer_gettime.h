f1c73bcc855b5addb9300231ceaced713a2d6aca
Olaf Mersmann <olafm@p-value.net>
2014-09-28 00:00:00 +0000

    version 1.4-2

--- src/nanotimer_gettime.h
+++ src/nanotimer_gettime.h
@@ -2,7 +2,7 @@
 
 static const nanotime_t nanoseconds_in_second = 1000000000LL;
 
-nanotime_t get_nanotime(void) {
+static nanotime_t get_nanotime(void) {
     struct timespec time_var;
 
     /* Possible other values we could have used are CLOCK_MONOTONIC,
@@ -10,7 +10,7 @@ nanotime_t get_nanotime(void) {
      * which, if I understand it correctly, would require the R
      * process to be bound to one core.
      */
-    clock_gettime(CLOCK_REALTIME, &time_var);
+    clock_gettime(CLOCK_MONOTONIC, &time_var);
 
     nanotime_t sec = time_var.tv_sec;
     nanotime_t nsec = time_var.tv_nsec;
